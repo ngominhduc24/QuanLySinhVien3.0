bba62e1a8ed08203e68bb08c8f04e7e4d2c1885b
duc minh <ngominhduc100@gmail.com>
2022-01-22 23:17:13 +0700

    + add LoginDAO,AccountDTO

---
+++ Winform_QuanLySinhVien3.0/DAL(data access layer)/LoginDAO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winform_QuanLySinhVien3._0.DTO_data_transfer_object_;
+
+namespace Winform_QuanLySinhVien3._0.DAO
+{
+    internal class LoginDAO
+    {
+        LoginDAO() { }
+
+        private static LoginDAO instance;
+
+        public static LoginDAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new LoginDAO();
+                return instance;
+            }
+        }
+
+        public int GetTypeAccount(string Username, string Password) 
+        {
+            if (CheckAccount(Username,Password))
+            {
+                object[] parameter = new object[] { Username, Password };
+                DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM Account WHERE Username = @Username  and Password = @Password ", parameter);
+                Account account = new Account(data.Rows[0]);
+                return account.Type;
+            }
+            return -1;
+        }
+
+        public bool CheckAccount(string Username, string Password)
+        {
+            object[] parameter = new object[] { Username, Password };
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM Account WHERE Username = @Username  and Password = @Password ", parameter);
+            int result = data.Rows.Count;
+            if (result > 0)
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ Winform_QuanLySinhVien3.0/DTO(data transfer object)/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winform_QuanLySinhVien3._0.DTO_data_transfer_object_
+{
+    internal class Account
+    {
+        public Account(DataRow Data)
+        {
+            this.username = Data["Username"].ToString();
+            this.password = Data["Password"].ToString();
+            this.type = (int)Data["Type"];
+            this.id = (int)Data["id"];
+        }
+
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int type;
+
+        public int Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+    }
+}
